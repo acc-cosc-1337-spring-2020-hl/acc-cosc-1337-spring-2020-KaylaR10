4f16d6c26168d8745951619fa72055a55f32f190
Kayla Reyes <kay@Kaylas-Air.lan>
2020-02-13 23:16:00 -0600

    decisons homework

--- src/homework/02_decisions/decisions.h
+++ src/homework/02_decisions/decisions.h
@@ -3,7 +3,7 @@
 
 //Write a function prototype named get_grade_points that accepts a string letter_grade and returns
 //an int.
-int get_grade_points(std::string letter_grades);
+int get_grade_points(std::string letter_grade);
 
 
 //Write a function prototype with name calculate_gpa that accepts an int named credit_hours and
