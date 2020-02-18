40061734133e99ef53947a12e6ac76a860afae30
KaylaR10 <60203825+KaylaR10@users.noreply.github.com>
2020-02-18 06:31:26 -0600

    Update for_ranged.cpp

--- src/examples/03_module/04_for_ranged/for_ranged.cpp
+++ src/examples/03_module/04_for_ranged/for_ranged.cpp
@@ -1,8 +1,5 @@
-#include "for_ranged.h"
-#include<iostream>
-#include<vector>
-using std::vector;
-using std::cout;
+include "for_ranged.h"
+
 
 /*
 Write code for loop_string_w_index that accepts a string parameter.
@@ -14,10 +11,9 @@ e
 s
 t
 */
-
-void loop_string_w_index(const string & str)
+void loop_string_w_index(const string &str) 
 {
-	for (int i = 0; i < str.length(); i++)
+	for (int i = 0; i < str.length(); ++i) 
 	{
 		cout << str[i] << "\n";
 	}
@@ -34,8 +30,7 @@ e
 s
 t
 */
-
-void loop_string_w_auto(const string & str)
+void loop_string_w_auto(const string &str)
 {
 	for (auto s : str)
 	{
@@ -43,13 +38,21 @@ void loop_string_w_auto(const string & str)
 	}
 }
 
-
 void loop_vector_w_index() 
 {
-	vector<int> nums = {9, 10, 99, 5,67 };
+	vector<int> nums = {9, 10, 99, 5, 67};
 
 	for (int i = 0; i < nums.size(); ++i) 
 	{
 		std::cout << nums[i] << "\n";
+	}
+}
+
+void loop_string_w_auto_ref(string & str)
+{
+	for (auto &s : str)
+	{
+		cout << s << "\n";
+		s = 'z';
 	}
 }
