317b40dc008e5deeb28a56a9bb75f0531886496b
KaylaR10 <60203825+KaylaR10@users.noreply.github.com>
2020-02-18 06:33:18 -0600

    Update main.cpp

--- src/examples/03_module/04_for_ranged/main.cpp
+++ src/examples/03_module/04_for_ranged/main.cpp
@@ -1,13 +1,14 @@
-
-#include<string>
-#include<iostream>
+ 
 #include "for_ranged.h"
 
 int main() 
 {
-	string str = "john";
-	loop_string_w_index(str);
+	string str = "test";
 
+	loop_string_w_index(str);
+	cout << str;
 	loop_string_w_auto(str);
+	cout << str;
+
 	return 0;
 }
