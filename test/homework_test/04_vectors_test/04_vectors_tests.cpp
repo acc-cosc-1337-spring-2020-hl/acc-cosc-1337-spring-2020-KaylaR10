ade1b90eb208ca9814cfc46c12af8d9c6ca79f64
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-03-04 21:37:53 -0600

    hw4 grading: use correct test data

--- test/homework_test/04_vectors_test/04_vectors_tests.cpp
+++ test/homework_test/04_vectors_test/04_vectors_tests.cpp
@@ -8,18 +8,29 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Test get_max_from_vector function")
 {
-    vector<int> numbers{ 22, 9, 45, 23, 6};
-    REQUIRE(get_max_from_vector(numbers)== 45);
+	vector<int> nums1 = { 3,8,1,99,1000 };
+	vector<int> nums2 = { 15,12,11,99,88 };
+	vector<int> nums3 = { 150,120,11,990,88888 };
+
+	REQUIRE(get_max_from_vector(nums1) == 1000);
+	REQUIRE(get_max_from_vector(nums2) == 99);
+	REQUIRE(get_max_from_vector(nums3) == 88888);
 }
 
 TEST_CASE("Test for is_prime funtion")
 {
-    REQUIRE(is_prime(2)==true);
-    REQUIRE(is_prime(4)==false);
+	REQUIRE(is_prime(2) == true);
+	REQUIRE(is_prime(4) == false);
+	REQUIRE(is_prime(43) == true);
+	REQUIRE(is_prime(44) == false);
+	REQUIRE(is_prime(45) == false);
 }
 
 TEST_CASE("Test vector_of_primes function")
 {
     vector<int> vec{ 2,3,5,7 };
+	vector<int> primes50 = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+
+	REQUIRE(vector_of_primes(50) == primes50);
     REQUIRE(vector_of_primes(10) == vec);
 }
