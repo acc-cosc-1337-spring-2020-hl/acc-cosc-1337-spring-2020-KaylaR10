530a0be4184a1767a24c9190aa1f013d9c1bc04b
KaylaR10 <kayla.reyes@g.austincc.edu>
2020-03-04 20:04:47 -0600

    CW6 overload

--- src/classwork/05_assign/rectangle.h
+++ src/classwork/05_assign/rectangle.h
@@ -1,3 +1,5 @@
+#include<iostream>
+using namespace std;
 /*
 Create the interface for a Rectangle class with one constructor that accepts two int parameters (width, height),
 has one public function area that returns the area of the rectangle, and one private function calculate_area
@@ -8,6 +10,7 @@ class Rectangl
 public:
     Rectangl(int w, int h): width{ w }, height { h } {}
     int get_Area();
+    friend std::ostream& operator<<(std::ostream& out, const Rectangl& rec);
 private:
     int calculate_Area();
     int width;
